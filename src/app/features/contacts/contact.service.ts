892a070f2b3a52b69ca30ed266ee4d0200901cd0
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-04-02 20:52:53 -0500

    feat:semana_4

---
+++ src/app/features/contacts/contact.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from "@angular/core";
+
+@Injectable({providedIn:'root'})
+
+
+export class ConctactService{
+
+  // Método para crear un nuevo contacto
+  newContact(){
+
+  }
+
+  // Método para obtener un contacto por id
+  getContactById(){
+
+  }
+
+  // Método para actualizar un contacto
+  updateContact(){
+
+  }
+
+  // Método para eliminar un contacto
+  deleteContact(){
+
+  }
+
+  // Método para obtener todos los contactos
+  getAllContacts(){
+
+  }
+
+}
